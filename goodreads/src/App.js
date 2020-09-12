f7db8ede537f66a058242ef7763c88592efe5d87
Michael Famurewa <myfamurewa@gmail.com>
2020-09-12 14:44:34 -0400

    connected to the api

--- goodreads/src/App.js
+++ goodreads/src/App.js
@@ -4,8 +4,15 @@ import './App.css';
 import axios from 'axios';
 
 function App() {
+  const key = process.env.REACT_APP_KEY
+  console.log("key", key)
+  const [bookState, setBookState] = useState()
   useEffect(() => {
-   axios.get()
+   axios.get(`https://cors-anywhere.herokuapp.com/https://www.goodreads.com/book/review_counts.json?key={${key}}&isbns=0596009208,0596517742
+   `)
+   .then(res => {
+     console.log("res", res)
+   })
   }, [])
   return (
    <div>
